5c20e55ece6f5baac65f6fe1cef2644af2212a00
DeletedMyEmail <88390464+DeletedMyEmail@users.noreply.github.com>
2025-02-12 18:04:57 +0100

    add git configs

--- init-dist.sh
+++ init-dist.sh
@@ -2,20 +2,36 @@
 sudo apt update
 yes | sudp apt upgrade
 
+
+# install tools
 # nala
 yes | sudo apt install nala
 sudo nala fetch
-
 # atuin
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+# git
+yes | sudo nala install git
 
-# replace .bashrc
-cp .bashrc ~/.bashrc
 
-# add scripts to bin
+# .bashrc
+cp bashrc ~/.bashrc
 
+
+# add scripts to bin --------------------------------
 for path in scripts/*.sh; do
 	filename=$(basename $path)
 	cp "scripts/${filename}" "/home/joshua/bin/${filename%.*}"
 	chmod +x "/home/joshua/bin/${filename%.*}"
 done
+
+
+# make dirs
+mkdir -p /home/joshua/Programming/Prv
+mkdir -p /home/joshua/Programming/Uni
+mkdir -p /home/joshua/Programming/Work
+
+# git config
+cp gitconfig_prv /home/joshua/Programming/Prv/.gitconfig
+cp gitconfig_uni /home/joshua/Programming/Uni/.gitconfig
+cp gitconfig_work /home/joshua/Programming/Work/.gitconfig
+cp gitconfig_global /home/joshua/.gitconfig
