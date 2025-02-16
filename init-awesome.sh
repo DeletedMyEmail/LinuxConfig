f5ed40a7f6bda4bac37dd56298dd31c6f70e69d6
DeletedMyEmail <88390464+DeletedMyEmail@users.noreply.github.com>
2025-02-16 17:03:01 +0100

    arch btw

--- init-awesome.sh
+++
@@ -1,7 +0,0 @@
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-yes | sudo nala install awesome
-cp awesome/ ~/.config/awesome -r
-yes | sudo nala install arandr compton nitrogen dmenu
-echo -e "${GREEN}Awesome wm installed and config files added ${NC}"
--- init-dist.sh
+++ init-dist.sh
@@ -2,64 +2,42 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 # sys update
-sudo apt update
-yes | sudo apt upgrade
-echo -e "${GREEN}System updated ${NC}"
-echo
-
-# install tools
-# nala
-yes | sudo apt install nala
-sudo nala fetch
-echo -e "${GREEN}Nala installed ${NC}"
-echo
-# curl
-yes | sudo nala install curl
-# atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-echo -e "${GREEN}Atuin installed ${NC}"
-echo
-# git
-yes | sudo nala install git
-echo -e "${GREEN}Git installed ${NC}"
-echo
-# flatpak
-yes | sudo nala install flatpak
-yes | flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install org.freedesktop.Platform
-# brave
-yes | flatpak install flathub com.brave.Browser
-echo -e "${GREEN}Brave installed ${NC}"
-echo
-# bitwarden
-yes | flatpak install flathub com.bitwarden.desktop
-echo -e "${GREEN}Bitwarden installed ${NC}"
-echo
-
-# .bashrc
-cp bashrc ~/.bashrc
-
-# make dirs
-mkdir ~/bin
-mkdir -p ~/Programming/Prv
-mkdir -p ~/Programming/Uni
-mkdir -p ~/Programming/Work
-
+sudo pacman -Syyu
+# yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# install ui stuff
+yay -S hyprland hyprpaper hyprlock wofi nwg-look waybar
+# inistall other tools
+yay -S kitty bitwarden yubioath-desktop snapd code blueman pavucontrol timeshift
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh # atuin
 
 # add scripts to bin
+mkdir ~/bin
 for path in scripts/*.sh; do
         filename=$(basename $path)
-        cp "scripts/${filename}" "~/bin/${filename%.*}"
-        chmod +x "~/bin/${filename%.*}"
+        cp scripts/${filename} ~/bin/${filename%.*}
+        chmod +x ~/bin/${filename%.*}
 done
 echo -e "${GREEN}Bash commands added ${NC}"
 
+# .config files
+for path in configs/*; do
+        path=$(basename $path)        
+        cp -r configs/${path} ~/.config/
+done
+echo -e "${GREEN}.config files added ${NC}"
+
 # git config
+mkdir -p ~/Programming/Prv
+mkdir -p ~/Programming/Uni
+mkdir -p ~/Programming/Work
 cp gitconfigs/gitconfig_prv ~/Programming/Prv/.gitconfig
 cp gitconfigs/gitconfig_uni ~/Programming/Uni/.gitconfig
 cp gitconfigs/gitconfig_work ~/Programming/Work/.gitconfig
 cp gitconfigs/gitconfig_global ~/.gitconfig
 echo -e "${GREEN}Git configured ${NC}"
 
-# reload
+# .bashrc
+cp bashrc ~/.bashrc
 . ~/.bashrc
+echo -e "${GREEN}.bashrc file added ${NC}"
